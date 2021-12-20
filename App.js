52f3c4b83071dfbccd55ee62d5191b53e72b1af6
Jirat Kaennak <mr.pkisz@gmail.com>
2021-12-20 13:49:32 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,28 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.textbold}>Mr.Jirat Kaennak</Text>
+      <Text>Mr.Jirat Kaennak</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#339999',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  textbold:{
+    color:'#fff',
+    fontSize: 20,
+    fontWeight: 'bold',
+    border: '5px solid red',
+  }
+});
